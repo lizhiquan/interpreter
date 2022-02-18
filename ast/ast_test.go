70e49555b97618a9de3aa0135fec3c50cac2e09f
Vincent Le <quyen.lechi@gmail.com>
2022-02-18 15:56:06 -0800

    feat(ast): convert ast back to string

---
+++ ast/ast_test.go
@@ -0,0 +1,30 @@
+package ast
+
+import (
+	"interpreter/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+
+	expected := "let myVar = anotherVar;"
+	actual := program.String()
+	if expected != actual {
+		t.Errorf("expected program.String() to be %s, got %s", expected, actual)
+	}
+}
